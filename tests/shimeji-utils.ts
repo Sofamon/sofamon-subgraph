a8818325c06a08ab257e63bd88c57241c1f472d9
ethanlolomg <ethanlolomg@gmail.com>
2023-03-02 10:21:54 -0800

    Initial commit

---
+++ tests/shimeji-utils.ts
@@ -0,0 +1,132 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  ApprovalForAll,
+  OwnershipTransferred,
+  TransferBatch,
+  TransferSingle,
+  URI
+} from "../generated/Shimeji/Shimeji"
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferBatchEvent(
+  operator: Address,
+  from: Address,
+  to: Address,
+  ids: Array<BigInt>,
+  amounts: Array<BigInt>
+): TransferBatch {
+  let transferBatchEvent = changetype<TransferBatch>(newMockEvent())
+
+  transferBatchEvent.parameters = new Array()
+
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("ids", ethereum.Value.fromUnsignedBigIntArray(ids))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam(
+      "amounts",
+      ethereum.Value.fromUnsignedBigIntArray(amounts)
+    )
+  )
+
+  return transferBatchEvent
+}
+
+export function createTransferSingleEvent(
+  operator: Address,
+  from: Address,
+  to: Address,
+  id: BigInt,
+  amount: BigInt
+): TransferSingle {
+  let transferSingleEvent = changetype<TransferSingle>(newMockEvent())
+
+  transferSingleEvent.parameters = new Array()
+
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return transferSingleEvent
+}
+
+export function createURIEvent(value: string, id: BigInt): URI {
+  let uriEvent = changetype<URI>(newMockEvent())
+
+  uriEvent.parameters = new Array()
+
+  uriEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromString(value))
+  )
+  uriEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+
+  return uriEvent
+}
